3bf13588e1dd0175e3964175cb08bf5387fb8ff9
Bresman-vargas <163463362+Bresman-vargas@users.noreply.github.com>
2024-08-29 00:39:38 -0400

    prueba 1

--- src/consulta_ciudaadasda/Consulta_Ciudaadasda.java
+++ src/consulta_ciudaadasda/Consulta_Ciudaadasda.java
@@ -15,6 +15,7 @@ public class Consulta_Ciudaadasda {
      */
     public static void main(String[] args) {
          System.out.println("Hola, Mundo");
+         System.out.println("Hola, Mundo 2 prueba bres");
     }
     
 }
